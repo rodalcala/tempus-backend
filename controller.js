747890ca051cefc67cb3994a96dd217ed4968779
Rodrigo Alcala <rodri.a90@gmail.com>
2019-02-27 16:59:00 +0100

    feat: implement changeStatus http request

--- controller.js
+++ controller.js
@@ -9,20 +9,27 @@ exports.getAllBoxes = async (ctx) => {
   }
 };
 
-// exports.changeStatus = async (ctx) => {
-//   const {
-//     id_, dataLeft, minsLeft, expiration, comments,
-//   } = ctx.request.body;
-//   try {
-//     Box.findByIdAndUpdate(id_, {
-//       dataLeft,
-//       minsLeft,
-//       expiration,
-//       comments,
-//     });
-//     ctx.status = 200;
-//   } catch (err) {
-//     console.log(err); // eslint-disable-line
-//     ctx.status = 500;
-//   }
-// };
+exports.changeStatus = async (ctx) => {
+  const {
+    dataLeft = 0, minsLeft = 0, expiration = '', comments = '',
+  } = ctx.request.body;
+  try {
+    const box = await Box.findById(ctx.params.id);
+    await Box.findByIdAndUpdate(ctx.params.id, {
+      // It's getting a deprecation warning, but if I change
+      // it to findOneAndUpdate I get the warning anyway 🤷‍
+      updated: new Date().toISOString(),
+      full: !box.full,
+      dataLeft,
+      minsLeft,
+      expiration,
+      comments,
+      $inc: { timesUpdated: 1 },
+    });
+    ctx.body = 'Box updated';
+    ctx.status = 200;
+  } catch (err) {
+    console.log(err); // eslint-disable-line
+    ctx.status = 500;
+  }
+};
--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const ctrl = require('./controller');
 const router = new Router();
 
 router.get('/boxes', ctrl.getAllBoxes);
-router.put('/box/:id');
+router.put('/box/:id', ctrl.changeStatus);
 // Missing the endpoints for users
 
 module.exports = router;
