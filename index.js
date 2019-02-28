b727621e99375c3bca2fc851e40a610e94e81c6f
Rodrigo Alcala <rodri.a90@gmail.com>
2019-02-28 10:24:16 +0100

    feat: add cors middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('koa-cors');
 require('./db');
 
 const app = new Koa();
@@ -8,6 +9,7 @@ const port = 4000;
 const router = require('./router');
 
 app.use(bodyParser());
+app.use(cors());
 app.use(router.routes());
 
 app.listen(port);
