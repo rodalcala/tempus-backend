dfca81256861cbd1ec0e08b847d9a4e0b835c954
Rodrigo Alcala <rodri.a90@gmail.com>
2019-04-03 18:32:09 +0200

    fix: change cors middleware to avoid deprecation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 const bodyParser = require('koa-bodyparser');
-const cors = require('koa-cors');
+const cors = require('@koa/cors');
 const router = require('./router');
 
 app.use(bodyParser());
